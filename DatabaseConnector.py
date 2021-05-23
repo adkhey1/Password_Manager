ac3aa5eb533cb2b3e262b6375e67816183ac524e
Simon <simon.carmesin@stud.fra-uas.de>
2021-05-23 17:53:49 +0100

    Datenbankverbindung programmiert

---
+++ DatabaseConnector.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+
+def connect_to_db():
+    connection = sqlite3.connect("Password_Database.db")
+    return connection
+
+
+def create_table(connection):
+    cursor = connection.cursor()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS
+      password(password_nr INTEGER PRIMARY KEY , title VARCHAR, username VARCHAR, password VARCHAR)""")
+
+
+def insert_data(connection, title, username, password):
+    cursor = connection.cursor()
+    cursor.execute("INSERT INTO password (title, username, password) VALUES (?, ?, ?)", (title, username, password))
+    connection.commit()
+
+
+def read_data(connection, password_nr):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM password WHERE password_nr = ?", password_nr)
+
+    return cursor.fetchall()
+
+
+def read_all(connection):
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM password")
+
+    return cursor.fetchall()
+
+
+def close_db_connection(connection):
+    connection.close()
--- main.py
+++ main.py
@@ -1,52 +1,4 @@
-import sqlite3
-
-connection = sqlite3.connect('Password_Database.db')
-
-cursor = connection.cursor()
-
-table1 = '''Create a Table if none exist
-  password(password_nr INTEGER PRIMARY KEY, title TEXT, username TEXT, password TEXT )'''
-
-cursor.execute(table1)
-
-
-        print("Press 'A' for a New Password")
-        print("Press 'B' to Delete a Password")
-        print("Press 'C' to change the Master Password\n")
-
-        decision = input("Choose your decision: ")
-
-        if decision == "A":
-
-            title = input("\nWrite down the Title: ")
-            username = input("Write down the Username: ")
-            password = input("Write down the Password: ")
-
-            #hier nochmal eine abfrage ob man wirklich das neue Passwort anlegen möchte
-
-            cursor.execute("INSERT INTO password VALUES" + title + username + password)
-
-        elif decision == "B":
-
-            choose = input("Write the Title of the deleted Password")
-
-            cursor.execute("DELETE FROM password WHERE choose ")
-
-        elif decision == "C":
-
-choose = input("Tip the new Master Key: ")
-
-cursor.execute("UPDATE password SET" + choose )
-
-        else:
-           print("Unknown decision")
-
-
-
-
-
-
-
+import DatabaseConnector as db
 
 
 def print_hi(name, alter):
@@ -54,19 +6,25 @@ def print_hi(name, alter):
     print(f'Hi, {name} ist {alter} Jahre alt')
 
 
-
 if __name__ == '__main__':
-
 
     print_hi('PyCharm', '17')
 
+    connection = db.connect_to_db()
+    db.create_table(connection)
+    db.insert_data(connection, 'test1', 'test2', 'test3')
+    print(db.read_data(connection, 1))
+
+    master_password = 'Key123'
+
     master_Password_Try = input("Login with your Master Password: ")
 
-    if Master_Password == master_Password_Try:
+    if master_password == master_Password_Try:
 
         print("Press 'A' for a New Password")
         print("Press 'B' to Delete a Password")
         print("Press 'C' to change the Master Password\n")
+
 
         decision = input("Choose your decision: ")
 
@@ -86,7 +44,5 @@ if __name__ == '__main__':
             print("Unknown decision")
 
 
-
     else:
         master_Password_Try = input("Login with your Master Password: ")
-
