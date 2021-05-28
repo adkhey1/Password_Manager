6ac1660ce93f858bb435c153812abf9acde95237
Simon <simon.carmesin@stud.fra-uas.de>
2021-05-28 14:34:57 +0100

    Masterpassword abfrage

--- main.py
+++ main.py
@@ -6,20 +6,25 @@ def print_hi(name, alter):
     print(f'Hi, {name} ist {alter} Jahre alt')
 
 
-def is_correct_master_password():
+def is_correct_master_password(i=0):
+    if i == 5:
+        return False
+
     master_password_try = input("Login with your Master Password: ")
 
     if master_password_try == master_password:
         return True
     else:
-        print('Master Passwort falsch')
-        return is_correct_master_password()
+        print('Incorrect Master Password')
+        i += 1
+        return is_correct_master_password(i)
 
 
 def new_password():
     title = input("\nWrite down the Title: ")
     username = input("Write down the Username: ")
     password = input("Write down the Password: ")
+    db.insert_data(connection, title, username, password)
 
 
 def delete_password():
@@ -42,8 +47,7 @@ if __name__ == '__main__':
 
     connection = db.connect_to_db()
     db.create_table(connection)
-    db.insert_data(connection, 'test1', 'test2', 'test3')
-    print(db.read_data(connection, 1))
+
 
     master_password = 'Key123'
 
@@ -70,3 +74,6 @@ if __name__ == '__main__':
 
         else:
             andere()
+
+    print(db.read_data(connection, 1))
+    print(db.read_all(connection))
