686e0d43e5d4ed7ee051e7ba668ff72fb9a1283b
Joannis123 <joannis.kopp@stud.fra-uas.de>
2021-06-04 14:44:10 +0200

    timer

--- ClipboardService.py
+++ ClipboardService.py
@@ -1,13 +1,13 @@
 import clipboard
+import time
 
 
 class ClipboardService:
 
     def copy_to_clipboard(self, text):
         clipboard.copy(text)
-        self.delete_after_seconds(30)
+        self.delete_after_seconds(20)
 
     def delete_after_seconds(self, seconds):
-        #TODO Start timer and after seconds call:
-        #self.copy_to_clipboard("")
-        pass
+        time.sleep(20)
+        self.copy_to_clipboard("")
