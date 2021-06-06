8dc04c1d9757ad0aea39b5ec54c2c2243eea1cc2
Simon <simon.carmesin@stud.fra-uas.de>
2021-06-06 13:50:20 +0100

    Implemented timer

--- ClipboardService.py
+++ ClipboardService.py
@@ -1,14 +1,16 @@
 import clipboard
-import time
+import threading
 
 
 class ClipboardService:
 
+    def callback(self):
+        self.copy_to_clipboard("")
+
     def copy_to_clipboard(self, text):
         clipboard.copy(text)
-        self.delete_after_seconds(20)
+        self.delete_after_seconds()
 
-    def delete_after_seconds(self, seconds):
-        time.sleep(20)
-        self.copy_to_clipboard("")
+    def delete_after_seconds(self):
+        threading.Timer(30, self.callback).start()
 
